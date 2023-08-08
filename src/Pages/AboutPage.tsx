86b69b96911faec3e3d747f1be14f4f9848f4de6
ElyasBelkhir <belkhirelyas@gmail.com>
2023-08-08 05:13:18 -0500

    typo

--- src/Pages/AboutPage.tsx
+++ src/Pages/AboutPage.tsx
@@ -4,8 +4,8 @@ function AboutPage() {
     
       <p>
         I am a student at The University of Texas at Dallas studying computer
-        science with focus in full-stack development and a passion for ML/AI.
-        I believe in solving problems that matter and am eager to ac this
+        science with a focus in full-stack development and a passion for ML/AI.
+        I believe in solving problems that matter and am eager to achieve this
         through software.
       </p>
     </div>
