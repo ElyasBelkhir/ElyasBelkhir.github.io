247b25a5e213440fd81095bf79ce83e5951fe6b5
ElyasBelkhir <belkhirelyas@gmail.com>
2023-08-08 05:21:11 -0500

    yo

--- src/Components/ProjectItem.tsx
+++ src/Components/ProjectItem.tsx
@@ -7,10 +7,10 @@ function ProjectItem({image, name, about, link}: { image: string; name: string; 
 
   <div className="card" style={{width: '18rem'}}>
     <img src={image} className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">{name}</h5>
-      <p className="card-text">{about}</p>
-      <a className="btn btn-primary"
+    <div className="card-body" >
+      <h5 className="card-title" >{name}</h5>
+      <p className="card-text" >{about}</p>
+      <a className="btn btn-primary" 
           href={link}
           target="_blank"
           rel="noopener noreferrer"
